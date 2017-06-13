5f54d500e0316ea726281415b8e2ed47568063e0
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-06-13 13:23:29 -0400

    Created game object to house dialogue manager

---
+++ Assets/Traverser/Scripts/DialogueManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+    #region Singleton
+    private static DialogueManager _instance;
+    public static DialogueManager Instance
+    {
+        get
+        {
+            if (_instance == null) {
+                GameObject dm = new GameObject("DialogueManager");
+                dm.AddComponent<DialogueManager>();
+
+            }
+            return _instance;
+        }
+
+    }
+    void Awake() {
+        _instance = this;
+
+    }
+    #endregion
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
